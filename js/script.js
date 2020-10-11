6ba08ac5aa3e89757a156001335cbe4b6eb5d52e
Daria <daria.bycova@yandex.ru>
2020-10-11 15:09:17 +0300

    third mess

--- js/script.js
+++ js/script.js
@@ -95,7 +95,8 @@ setClock('timer', deadLine);
 
 let more = document.querySelector('.more'),
 overlay = document.querySelector('.overlay'),
-close = document.querySelector('.popup-close');
+close = document.querySelector('.popup-close'),
+descriptionBtn = document.querySelectorAll('.description-btn');
 
 more.addEventListener('click', function() {
     overlay.style.display = 'block';
@@ -103,9 +104,20 @@ more.addEventListener('click', function() {
     document.body.style.overflow = 'hidden';
 })
 
+for(let i = 0; i<=descriptionBtn.length; i++) {
+descriptionBtn[i].addEventListener('click', function() {
+    overlay.style.display = 'block';
+    this.classList.add('more-splash');
+    document.body.style.overflow = 'hidden';
+})}
+
+
 close.addEventListener('click', function() {
     overlay.style.display = 'none';
     more.classList.remove('more-splash');
+    for(let i = 0; i<=descriptionBtn.length; i++){
+        descriptionBtn[i].classList.remove('more-splash');
+    }
     document.body.style.overflow = '';
 })
 
